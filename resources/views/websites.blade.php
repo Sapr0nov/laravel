2b5278b66b34be96079ff591e9546ac3ba579b01
Sergey Sapronov <1262708@mail.ru>
2023-09-14 07:06:35 +0300

    init laravel

---
+++ resources/views/websites.blade.php
@@ -0,0 +1,342 @@
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Stack Site</title>
+	<base href="https://stacksite.ru/" />
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" href="./assets/css/style.css" type="text/css" media="all">
+	<script src="/assets/js/SmoothScroll.js"></script>
+        <script src="/assets/js/main.js"></script>
+	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<!-- Yandex.Metrika counter --> 
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter86984718 = new Ya.Metrika({
+                    id:86984718,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script> 
+
+<noscript><div><img src="https://mc.yandex.ru/watch/86984718" style="position:absolute; left:-9999px;" alt="" /></div></noscript> 
+<!-- /Yandex.Metrika counter -->
+
+</head>
+
+<body>
+
+    <div class="level0"></div>
+    <div id="animated-bg"></div>
+    <header>
+    <nav class="main-nav">
+        <ul class="main-nav__primary">
+            <li class="nav-item"><a class="nav-item-link" href="/blog/">Статьи</a></li>
+            <li class="nav-item"><a class="nav-item-link" href="/hobby/">Хобби</a></li>
+            <li class="nav-item"><a class="nav-item-link" href="#slide20">Работы</a></li>
+            <li class="nav-item"><a class="nav-item-link" href="/websites.html#slide22">Верстка</a></li>
+            <li class="nav-item"><a class="nav-item-link" href="#contacts">Контакты</a></li>
+            <li id="checkTheme"> <input id="darkTheme" type="checkbox"><label for="darkTheme"> </label></li>
+        </ul>
+    </nav>
+    </header>
+
+<div id="anim_block" class="animsition1">
+	<div class="anim_slides_wrap">
+		<section class="anim_block_slide anim_home">
+			<div class="container-fluid">
+				<div class="row">
+                    
+                    <div class="wrapper">
+                            <div class="column-1">
+                                <div class="bio-image-wrapper">
+                                    <div class="bio-image">
+                                        <img class="bio-image-image" src="/assets/img/common/avatar.jpg" loading="eager" width="100" height="100" alt="Segey Sapronov">
+                                    </div>
+                                </div>
+                           </div>
+                           <div class="column-2">
+                               <div>
+                                    <h1>Создание сайтов и web приложений</h1>
+                                    <p>Практически все веб приложения состоит из двух частей: backend (бэкенд) и frontend (фронтенд). Так и программистов делят на бэкенд и фронтэнд-разработчиков.</p>
+<span class="readmore"><a class="button anim_block_button" href="/blog/article.html">Читать далее..</a></span>
+                           </div>
+                           <div class="column-3">
+                                <img src="./assets/img/common/scroll.gif" style="width: 80%; max-width: 300px; margin: auto;" />
+                           </div>
+                    </div>
+
+		</div>
+	</div>
+</section>
+
+        <section class="anim_block_slide" id="slide1">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/three.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Game 3 in row</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">JS ES6, Cookie, REST API (php server, MySQL) canvas Phaser 3</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/three/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide2">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/adsPlayable.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">ads Playable</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">Тестовый пример создания интерактивных рекламных мини игр</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/adsPlayable/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide3">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/qrList.png" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">QR list</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">Сайт для создание списков с фото и генерацией QR - кода этого списка</p>
+                        </div>
+                        <a href="https://qr.stacksite.ru/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide4">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/unity1.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Game на Unity</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">Простая игра на Unity</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/unity1/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/racing.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Racing</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">Простая гонка на CSS и JS</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/racing/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide6">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/rendzuOne.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Рендзю</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">Игра в рендзю с одного компьютера</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/rendzuOne/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide7">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/ArcSky.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">ArcSky</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">JS и CSS Арканоид, космическая стрелялка</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/skyArc/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slide8">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/projects/runner.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Runner</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1">3D runner, с использованием библиотеки ThreeJS</p>
+                        </div>
+                        <a href="https://stacksite.ru/assets/projects2/runner/" class="button anim_block_button">перейти на сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><section class="anim_block_slide" id="slideContact">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="anim_slides" style="transform: scale(1); opacity: 0; z-index: 9;">
+                <div class="anim_slide" style="transform: translateX(-1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_img">
+                        <img src="assets/img/common/contact.jpg" alt="preview">
+                    </div>
+                </div>
+                <div class="anim_slide" style="transform: translateX(1271.5px); box-shadow: rgba(0, 0, 0, 0) 0px 0px 40px;">
+                    <div class="anim_block_text_wrap">
+                        <div class="anim_block_title">Контакты</div>
+                        <div class="anim_block_text">
+                            <p class="p1">
+                                <span class="s1"> telegram: <a href="tg://resolve?domain=@SapronovSergey">@SapronovSergey  </a><br/>e-mail: <a href="mailto:admin@stacksite.ru">admin@stacksite.ru</a></p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+  	
+	<nav><ul class="anim_vertical_nav">
+            <li><a href="/#0" class="anim_prev"></a></li>
+            <li><a href="/#0" class="anim_next"></a></li>
+        </ul>
+    </nav>
+
+        <footer id="footer">
+            <div class="container" id="contacts">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="socials">
+                            <ul>
+                                <li><a href="/assets/licenses/SapronovFullStackDeveloper.pdf"><span>CV (резюме)</span></a></li>
+                                <li><a href="/assets/licenses/englishB1.pdf"><span>English B1</span></a></li>
+                                <li><a href="/assets/licenses/сертификат PHP 2.jpg"><span>PHP</span></a></li>
+                                <li><a href="/assets/licenses/сертификат ASP.jpg"><span>ASP.NET</span></a></li>
+                                <li><a href="/assets/licenses/"><span>SQL</span></a></li>
+                                <li><a href="/assets/licenses/ES6 Lectrum 2020.jpg"><span>ES6 (JavaScript)</span></a></li>
+                                <li><a href="/assets/licenses/сертификат св-во СЕО.jpg"><span>SEO</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row jul-container">
+                    <div class="col-xs-12">
+                        <div class="link">
+                            <a href="https://github.com/Bezumstvo"><img src="/assets/img/common/github-icon.svg" width="48" alt="github">GitHub</a>
+                            <a href="https://www.linkedin.com/in/sapronov-sa/"><img src="/assets/img/common/linkedin-icon.png" width="48" alt="linkedin">LinkedIn</a>
+                            <a href="https://codepen.io/bez-cepera"><img src="/assets/img/common/codepen-icon.svg" width="48" alt="codepen">CodePen</a>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12">
+                        <div class="copyright">
+                           2008-2023 (с) <br class="visible-xs"> Stack Sites 
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+
+	
+<script type="text/javascript" src="./assets/js/libs.min.js"></script>
+<script type="text/javascript" src="./assets/js/common.js"></script>
+</body>
+</html>
