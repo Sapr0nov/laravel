265db2b8809ddbce97306d9d70ea31ce0e857a18
Sergey Sapronov <1262708@mail.ru>
2023-09-19 11:33:17 +0300

    rename migration note

--- database/migrations/2023_09_19_000000_create_notes_table.php
+++ database/migrations/2023_09_19_000000_create_notes_table.php
@@ -10,12 +10,22 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up() { 
+        Schema::dropIfExists('notes');
         Schema::create('notes', function (Blueprint $table) { 
             $table->id();
             $table->string('name', 250);
             $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps(); 
             $table->index('created_at');
         }); 
-    }  
+    }
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
 };
